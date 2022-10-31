7190b7b0f4ef5fcca94a66b357dec3d2a754bd8d
JaminthonEnriqueRuedaLarrotaADSO <pocopoc4158j@gmail.com>
2022-10-30 23:12:20 -0500

    Ejercicio 4

--- main.js
+++ main.js
@@ -1,117 +1,40 @@
-addEventListener("DOMContentLoaded", (e)=>{
+addEventListener("DOMContentLoaded", (e) =>{
 
-let aprobar = document.querySelector('#Respuesta')
-let form = document.querySelector('#formulario')
-let ac = 0
-form.addEventListener('submint', (e) => {
-e.preventDefault();
-let datos = Object.fromEntries(new FormData(e.target))
-let numer = datos.numero
-for (let i = 0; i <= numer/2; i++) {
-    if (numero % i == 0) {
-        ac += i
-    }
-    if (ac==numer) {
-        let si = "numero perfecto"
-        aprobar.innerHTML = si
-    }
-    else{
-        let no = "no es un numero perfecto"
-        aprobar.innerHTML = no
-    }
-}
-})
-// no usar.............................................
-
-
-//     // let resultado = document.querySelector('#Resultado')
-//     // let acum = 0
-//     // let numer = parseInt(document.getElementById('enviar').value)
-//     // for (let i = 0; numer ; index++) {
-//     //     if (numero % i == 0) {
-//     //         acum += i
-            
-//     //     }    
-//     //     if (acum == 28) {
-//     //         let es = "es perfecto"
-//     //         resultado.innerHTML = es;
-//     //     }
-        
-//     //     else {
-//     //         resultado.innerHTML =
-//     //         "no es perfecto"
-//     //     }
-//     // }
-//     // console.log(numer)
+    let button = document.getElementById('boton');
+    let respuesta = document.getElementById('respuesta');
+    let respuesta1 = document.getElementById('respuesta1');
     
-
-
-
-
-
-
-
-
-
-
-// function cambio() {
-//     let acum = 0
-//     // let numer = parseFloat(document.getElementById('enviar').value)
-//     for (let i = 0; 28 ; index++) {
-//         if (28 % i == 0) {
-//             acum += i
-            
-//         }    
-//         if (acum == 28) {
-//             let es = "es perfecto"
-//             // resultado.innerHTML = es;
-//         }
-        
-//         else {
-//             let noes = "no es perfecto"
-//             // resultado.innerHTML =
-//             // "no es perfecto"
-//         }
-//     }
-//     document.getElementById('Resultado').innerHTML = es;
-// }
-
-// document.getElementById('enviar').onclick = 
-// function ()
-// {
-// cambio();
-// }
-// console.log(document.getElementById('enviar').value)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// punto2    
-    // function cambio() {
-    //     let horas = parseFloat(document.getElementById('numero').value);
-    //     let resultado = 5.208 *horas
-    //     document.getElementById('prueba').innerHTML= "Su salario es de : " + resultado + " pesos :) tenga buendia.";
-        
-    // }
-
-    // document.getElementById('enviar').onclick = function() {
-    // cambio();
-    // }
+    button.addEventListener('click', solucion())
     
-
+    function solucion() 
+    {
+        let venta1 = parseInt(document.getElementById('venta1').value);
+        let venta2 = parseInt(document.getElementById('venta2').value);
+        let venta3 = parseInt(document.getElementById('venta3').value);
+        let SueldoBase = parseInt(document.getElementById('sueldoBase').value);
+        let calculo1 = (venta1+venta2+venta3)*0.10
+        let calculo2 =  (calculo1 + SueldoBase)
     
-
-
-})
+        respuesta.innerHTML=`El resultado de las comisiones es de: ${calculo1}`
+        respuesta1.innerHTML= `El resultado de el sueldo mas las comisiones es de: ${calculo2}`
+    }
+    
+    
+    
+        // function cambio() {
+        //     let venta1 = parseInt(document.getElementById('venta1').value);
+        //     let venta2 = parseInt(document.getElementById('venta2').value);
+        //     let venta3 = parseInt(document.getElementById('venta3').value);
+        //     let SueldoBase = parseFloat(document.getElementById('sueldoBase').value);
+        //     let calculo1 = (venta1+venta2+venta3)*0.10
+        //     let calculo2 =  (calculo1 + SueldoBase)
+        //     // document.getElementsByTagName('p').innerHTML=  "El dinero por sus comisiones es de: " + calculo1;
+        //     document.getElementById('prueba1').innerHTML =  "El dinero por sus comisiones y sueldo base es de :" + calculo2;
+        // }
+    
+        // document.getElementsByClassName('enviar').onclick = function() {
+        // cambio();
+        // }
+        
+    
+    })
